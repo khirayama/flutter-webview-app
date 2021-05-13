955b298ce12c939f04fea0c89c7ac332dfc83307
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2021-05-14 07:28:16 +0900

    Format webview message

--- lib/webviewmessage.dart
+++ lib/webviewmessage.dart
@@ -4,16 +4,16 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 JavascriptChannel onMessageReceived(Function callback) {
   return JavascriptChannel(
-      name: "app",
-      onMessageReceived: (JavascriptMessage event) {
-        var payload = jsonDecode(event.message);
-        callback(payload);
-      },
+    name: "app",
+    onMessageReceived: (JavascriptMessage event) {
+      var payload = jsonDecode(event.message);
+      callback(payload);
+    },
   );
 }
 
 void postMessage(WebViewController webViewController, dynamic payload) {
   webViewController.evaluateJavascript('''
-      window.dispatchEvent(new CustomEvent("app:message", { detail: ${jsonEncode(payload)}}));
-      ''');
+    window.dispatchEvent(new CustomEvent("app:message", { detail: ${jsonEncode(payload)}}));
+  ''');
 }
